16fbb5ff64720b12a3901894c851d94960d342e2
gtblackmore <gtblackmore@gmail.com>
2020-04-07 21:15:48 -0400

    create landlord user table

---
+++ db/migrate/20200408011413_create_landlord_user.rb
@@ -0,0 +1,10 @@
+class CreateLandlordUser < ActiveRecord::Migration[6.0]
+  def change
+    create_table :landlord_user do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :password_database
+    end
+  end
+end
